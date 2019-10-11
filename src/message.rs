941b8bfa0ebb8dd6ac12d7eb706deae0c8e3adcb
Michele Lacchia <michelelacchia@gmail.com>
2019-10-11 19:53:54 +0200

    chore: add test for MessageGuard::payload()

--- src/message.rs
+++ src/message.rs
@@ -177,4 +177,23 @@ mod tests {
             err
         );
     }
+
+    #[test]
+    fn payload_field_is_accessible() {
+        let client = redis::Client::open("redis://127.0.0.1:6379/").unwrap();
+        let con = client.get_connection().unwrap();
+
+        let bm = BrokenMessage{};
+        let p = vec![1, 2, 3, 4];
+        let rc = RefCell::new(con);
+        let mg = MessageGuard::new(
+            bm,
+            p,
+            &rc,
+            "proc_test".into(),
+            "unack_test".into(),
+        );
+
+        assert_eq!(Vec::from(mg.payload()), vec![1, 2, 3, 4]);
+    }
 }
